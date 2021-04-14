cb56222fef324713059d3bd0b5544407357cabb2
AlexQuigley1 <alexjquigley@hotmail.com>
2021-04-14 16:06:49 +0100

    loads index

--- src/main/java/com/NovusBank/application/StartUp.java
+++ src/main/java/com/NovusBank/application/StartUp.java
@@ -2,6 +2,8 @@ package com.NovusBank.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.NovusBank.web.WebController;
@@ -9,7 +11,12 @@ import com.NovusBank.web.WebController;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses=WebController.class)
-public class StartUp {
+public class StartUp extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StartUp.class);
+    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(StartUp.class, args);
