f64d250f17592479ee5191b2f9db4ce3b5820d01
Aya10 <47397624+Aya10@users.noreply.github.com>
2021-04-20 14:37:09 +0100

    final selenium commit

--- src/main/java/test/SelTesting.java
+++ src/main/java/test/SelTesting.java
@@ -2,41 +2,32 @@ package test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class Selenium {
+public class SelTesting {
 
 	
 	
 
 	
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "/Users/ayaaboutwerat/Downloads/chromedriver3");
+		System.setProperty("webdriver.chrome.driver", "/Users/ayaaboutwerat/desktop/NovusBankApp/src/test/chromedriver");
 		
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://localhost8090/");
-		driver.findElement(By.xpath("email")).click();	
-		driver.findElement(By.xpath("")).click();			
-		driver.findElement(By.xpath("")).click();		
+		driver.get("http://127.0.0.1:8090");
+		driver.manage().window().maximize();
 		
-		driver.findElement(By.xpath("")).click();
-
-
-
-		driver.findElement(By.xpath("")).click();
-		
+		driver.findElement(By.id("email")).sendKeys("aya@gmail.com");	
+		driver.findElement(By.id("password")).sendKeys("123456");	
+		driver.findElement(By.xpath("submit")).click();
+//		
 //		//maximize window
 
 //		driver.findElement(By.id("")).click();	
 //		for(int i = 0; i < 6; i++) 		
 //			
 			//driver.findElement(By.xpath("")).click();				
-//		
-		System.out.println(driver.findElement(By.xpath("")).getText());
-
-		driver.manage().window().maximize();
-	
+////		
+//		System.out.println(driver.findElement(By.xpath("")).getText());
 		
-		
-		//driver.findElement(By.xpath("")).click();
 		//*[@id="password"]
 		//closing the browser
 		//driver.close();
